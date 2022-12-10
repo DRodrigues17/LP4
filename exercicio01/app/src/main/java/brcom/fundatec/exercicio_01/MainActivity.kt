c7cb834b4b562beccb8aacb5dd7a7845fec8ced2
drodrigues17 <adanielluiz71@gmail.com>
2022-12-10 12:04:49 -0300

    exercicio 1 terminado

--- exercicio01/app/src/main/java/brcom/fundatec/exercicio_01/MainActivity.kt
+++ exercicio01/app/src/main/java/brcom/fundatec/exercicio_01/MainActivity.kt
@@ -4,20 +4,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.bt_limpar).setOnClickListener { limparInput() }
+        findViewById<Button>(R.id.bt_limpar).setOnClickListener {
+            limparInput()
+            findViewById<Button>(R.id.bt_ok).setOnClickListener {
+                adcionarTextoFrase()
+            }
+        }
     }
 
-    fun limparInput(){
+    fun limparInput() {
         val nome = findViewById<EditText>(R.id.et_digite_aqui)
         nome.text.clear()
     }
 
-    fun adcionarTextoFrase(){
+    fun adcionarTextoFrase() {
+        val nome = findViewById<EditText>(R.id.et_digite_aqui)
+        val frase = findViewById<TextView>(R.id.tv_frase)
+
+        frase.setText("oi ${nome.text}, nome legal")
+        nome.text.clear()
 
     }
 }
